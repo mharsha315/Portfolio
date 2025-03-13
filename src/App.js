9fa462aa17929a3d7b22c799b107b317cbf042ad
Harshavardhana Mudduluru <55201451+mharsha315@users.noreply.github.com>
2025-03-13 18:06:10 -0400

    update resume

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Link as ScrollLink } from 'react-scroll';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Education from './components/Education';
-import Contact from './components/Contact';
-import './App.css';
-import resume from './Assets/Harshavardhana_Mudduluru_.pdf'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import "./App.css";
+import resume from "./Assets/Harshavardhana_Mudduluru.pdf";
 
 function App() {
   return (
@@ -22,14 +22,46 @@ function App() {
           <div className="navbar-right">
             <ul>
               {/* Use ScrollLink from react-scroll for smooth scrolling */}
-              <li><ScrollLink to="home" smooth={true} duration={100}>Home</ScrollLink></li>
-              <li><ScrollLink to="projects" smooth={true} duration={100}>Projects</ScrollLink></li>
-              <li><ScrollLink to="about" smooth={true} duration={100}>About Me</ScrollLink></li>
-              <li><ScrollLink to="skills" smooth={true} duration={100}>Skills</ScrollLink></li>
+              <li>
+                <ScrollLink to="home" smooth={true} duration={100}>
+                  Home
+                </ScrollLink>
+              </li>
+              <li>
+                <ScrollLink to="projects" smooth={true} duration={100}>
+                  Projects
+                </ScrollLink>
+              </li>
+              <li>
+                <ScrollLink to="about" smooth={true} duration={100}>
+                  About Me
+                </ScrollLink>
+              </li>
+              <li>
+                <ScrollLink to="skills" smooth={true} duration={100}>
+                  Skills
+                </ScrollLink>
+              </li>
               {/* Make sure 'education' matches the id of the Education section */}
-              <li><ScrollLink to="education" smooth={true} duration={100}>Education</ScrollLink></li> 
-              <li><ScrollLink to="contact" smooth={true} duration={100}>Contact</ScrollLink></li>
-              <li><a href={resume} className="resume-btn" download="Harshavardhana_Mudduluru_Resume.pdf">Resume</a></li>
+              <li>
+                <ScrollLink to="education" smooth={true} duration={100}>
+                  Education
+                </ScrollLink>
+              </li>
+              <li>
+                <ScrollLink to="contact" smooth={true} duration={100}>
+                  Contact
+                </ScrollLink>
+              </li>
+              <li>
+                <a
+                  href={resume}
+                  className="resume-btn"
+                  download="Harshavardhana_Mudduluru_Resume.pdf"
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
           </div>
         </nav>
